e644b959ff46a925f512a5ca4ac83f173ddad656
Safaiya SUMODHEE <ssumodhe@e2r6p17.42.fr>
2017-02-17 16:13:46 +0100

    GNL avec multi fd mais pb buff 15

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,70 @@
 /*   By: ssumodhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/12 15:24:38 by ssumodhe          #+#    #+#             */
-/*   Updated: 2017/02/15 18:52:27 by ssumodhe         ###   ########.fr       */
+/*   Updated: 2017/02/17 15:36:17 by ssumodhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+t_gnl		*ft_fd_find_create(int	fd, t_gnl	**file)
+{
+	t_gnl	*tmp;
+	t_gnl	*list;
+
+	//	if (fd <= 2)
+	//		return(-404); //error
+
+	tmp = *file;
+	//	(lstdel(file) ???)
+	if(tmp)
+	{
+//		printf("YOP\n");
+		//while (tmp != NULL)
+		/*while (tmp->next != NULL)
+		{
+			list = tmp;
+			if (fd == list->fd)
+				return (list);
+			tmp = tmp->next;
+		}*/
+		while (tmp && (tmp->fd != fd))
+			tmp = tmp->next;
+
+		/*if (tmp == NULL)
+		{
+			return (list);
+		}
+		printf("YOPilait\n");
+		if (tmp->next == NULL)*/
+		if (tmp == NULL)
+		{
+			tmp = *file;
+			while (tmp->next != NULL)
+				tmp = tmp->next;
+			if ((list = (t_gnl *)malloc(sizeof(*list))) == NULL)
+				return (NULL);
+			list->fd = fd;
+			list->stock = NULL;
+			list->next = NULL;
+			tmp->next = list;
+			return (tmp->next);
+		}
+		else if (tmp->fd == fd)
+			return (tmp);
+	}
+	else
+	{
+		if ((list = (t_gnl *)malloc(sizeof(*list))) == NULL)
+			return (NULL);
+		list->fd = fd;
+		list->stock = NULL;
+		list->next = NULL;
+		*file = list;
+		return (*file);
+	}
+	return (NULL);
+}
 
 int		ft_while_read(char *buff, char **line, char **stock)
 {
@@ -48,8 +107,10 @@ int		ft_while_read(char *buff, char **line, char **stock)
 			*line = ft_strdup(tmp);
 			ft_strdel(&tmp);
 		}
+//	printf(CYAN "Dans while_read --> Stock = %s\n" RESET, *stock);
 		ft_strdel(stock);
 		*stock = ft_strdup(ft_strchr(buff, '\n') + 1);
+//	printf(CYAN "Dans while_read --> Stock = %s\n" RESET, *stock);
 		if ((*stock)[0] == '\0')
 			ft_strdel(stock);
 			//*stock = NULL;
@@ -63,16 +124,19 @@ int		ft_if_stock(char **stock, char **line)
 	int		i;
 	char 	*tmp;
 
+//	printf(GREEN "Dans if_stock --> Stock = %s\n" RESET, *stock);
 	if (!(*stock))
 		return (0);
 	if (ft_strchr(*stock, '\n') == NULL)
 	{
+//	printf(GREEN"Dans if_stock dans if--> Stock = %s\n" RESET, *stock);
 		*line = ft_strdup(*stock);
 		ft_strdel(stock);
 		//*stock = NULL;
 	}
 	else
 	{
+//	printf(GREEN"Dans if_stock dans else --> Stock = %s\n" RESET, *stock);
 		i = 0;
 		while ((*stock)[i] != '\n')
 			i++;
@@ -88,16 +152,37 @@ int		ft_if_stock(char **stock, char **line)
 	return (0);
 }
 
+/*void	ft_putlist(t_gnl *list)
+{
+	while (list)
+	{
+		ft_putstr("fd = ");
+		ft_putnbr(list->fd);
+		ft_putstr(" | stock  = ");
+		ft_putendl(list->stock);
+		ft_putstr("\n");
+		list = list->next;
+	}
+}*/
+
 int		get_next_line(const int fd, char **line)
 {
 	int			octet;
 	char		*buff;
-	static char	*stock;
+//	static char	*stock;
+	static t_gnl	*file;
+	t_gnl			*tmp_file;
 
 	if (fd < 0 || !line)
 		return (-1);
+//	ft_putstr("LIST :\n");
+//	ft_putlist(file);
 	*line = NULL;
-	if (ft_if_stock(&stock, line) == 1)
+	tmp_file = ft_fd_find_create(fd, &file);
+//	printf("FD de la liste = %d\n", file->fd);
+//	printf("STOCK de la liste = %s\n" RESET, file->stock);
+//	printf("STOCK  = %s\n" RESET, stock);
+	if (ft_if_stock(&tmp_file->stock, line) == 1)
 		return (1);
 	if (!(buff = ft_strnew(BUFF_SIZE)))
 		return (-1);
@@ -109,8 +194,11 @@ int		get_next_line(const int fd, char **line)
 			return (-1);
 		}
 		buff[octet] = '\0';
-		if (ft_while_read(buff, line, &stock) == 1)
+//	printf(RED"read --> Buff = %s\n" RESET, buff);
+		if (ft_while_read(buff, line, &tmp_file->stock) == 1)
 		{
+//			printf(RED"read --> Stock = %s\n" RESET, file->stock);
+	//		stock = ft_strdup(file->stock);
 	//		ft_strdel(&buff);
 			return (1);
 		}
